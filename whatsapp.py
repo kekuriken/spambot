8fc93af24b87583fcc09c159b70dc138acdf45d6
kekuriken <136371783+kekuriken@users.noreply.github.com>
2023-06-12 23:27:35 +0530

    Add files via upload

---
+++ whatsapp.py
@@ -0,0 +1,45 @@
+from sre_constants import CHCODES
+from turtle import done
+import pyautogui
+import time
+time.sleep(1)
+
+for i in range(1000):
+
+    pyautogui.typewrite("anime ke chode")
+    pyautogui.press('enter')
+    time.sleep(0.1)
+
+    from sre_constants import CHCODES
+from turtle import done
+import pyautogui
+import time
+time.sleep(1)
+
+for i in range(1000):
+
+    pyautogui.typewrite("anime ke chode")
+    pyautogui.press('enter')
+    time.sleep(0.1)
+    
+
+
+
+from sre_constants import CHCODES
+from turtle import done
+import pyautogui
+import time
+time.sleep(1)
+
+for i in range(1000):
+
+    pyautogui.typewrite("anime ke chode")
+    pyautogui.press('enter')
+    time.sleep(0.1)
+    
+
+
+
+    
+
+
